5bd5fc2be007b6e155ea2872e95d41bec93d0f5d
cliffootieno <clifford242526@gmail.com>
2024-10-26 15:08:55 +0300

    chore:"Removed static styles.css file, artists.html template, and main.go code for fetching and rendering artists data; updated main.go to use handler package and changed template directory; removed GetArtists function and updated HTTP handlers."

---
+++ handler/handler.go
@@ -0,0 +1,73 @@
+package handler
+
+import (
+	"encoding/json"
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+)
+
+var (
+	templates     *template.Template
+	artists_url   = "https://groupietrackers.herokuapp.com/api/artists"
+	locations_url = "https://groupietrackers.herokuapp.com/api/locations"
+)
+
+// struct model for artist's details, fetched using json tags
+type Artist struct {
+	Id           int      `json:"id"`
+	Image        string   `json:"image"`
+	Name         string   `json:"name"`
+	Members      []string `json:"members"`
+	CreationDate int      `json:"creationDate"`
+	FirstAlbum   string   `json:"firstAlbum"`
+}
+type locations struct {
+	Locations []string `json:"locations"`
+}
+
+var template_dir = "./web/templates/"
+
+func Fetch(url string) []byte {
+	body := []byte{}
+	var body_err error
+
+	response, artists_err := http.Get(url)
+	if artists_err != nil {
+		log.Fatal("The following error was encountered while making a get request to the groupie tracker api: %s", artists_err)
+		return nil
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode == http.StatusOK {
+		body, body_err = io.ReadAll(response.Body)
+		if body_err != nil {
+			log.Fatal(body_err)
+			return nil
+		}
+	}
+
+	return body
+}
+
+// GetArtists fetches all the artists from the api and stores them in an array of objects
+func GetArtists(w http.ResponseWriter, r *http.Request) {
+	artists := []Artist{}
+
+	artists_bytes := Fetch(artists_url)
+
+	json.Unmarshal(artists_bytes, &artists)
+
+	templates.ExecuteTemplate(w, "artists.html", artists)
+}
+
+func GetLocations(w http.ResponseWriter, r *http.Request) {
+	var locationsSlice locations
+	fetchedLocations := Fetch(locations_url)
+
+	json.Unmarshal(fetchedLocations, locationsSlice)
+
+	templates.ExecuteTemplate(w, "artists.html", fetchedLocations)
+}
--- main.go
+++ main.go
@@ -1,72 +1,33 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"html/template"
-	"io"
 	"log"
 	"net/http"
+
+	"groupie/handler"
 )
 
 var (
 	templates    *template.Template
-	template_dir = "templates/"
-	artists_url  = "https://groupietrackers.herokuapp.com/api/artists"
+	template_dir = "/web/templates/"
 )
 
-// struct model for artist's details, fetched using json tags
-type Artist struct {
-	Id           int      `json:"id"`
-	Image        string   `json:"image"`
-	Name         string   `json:"name"`
-	Members      []string `json:"members"`
-	CreationDate int      `json:"creationDate"`
-	FirstAlbum   string   `json:"firstAlbum"`
-}
-
-func Fetch(url string) []byte {
-	body := []byte{}
-	var body_err error
-
-	response, artists_err := http.Get(url)
-	if artists_err != nil {
-		log.Printf("The following error was encountered while making a get request to the groupie tracker api: %s", artists_err)
-		return nil
-	}
-
-	defer response.Body.Close()
-
-	if response.StatusCode == http.StatusOK {
-		body, body_err = io.ReadAll(response.Body)
-		if body_err != nil {
-			log.Printf("The following error was encountered while reading the response body: %s", body_err)
-			return nil
-		}
-	}
-
-	return body
-}
-
-// GetArtists fetches all the artists from the api and stores them in an array of objects
-func GetArtists(w http.ResponseWriter, r *http.Request) {
-	artists := []Artist{}
-
-	artists_bytes := Fetch(artists_url)
-
-	json.Unmarshal(artists_bytes, &artists)
-
-	templates.ExecuteTemplate(w, "artists.html", artists)
-}
-
 func main() {
+	// var err error
 	templates, _ = templates.ParseGlob(template_dir + "*.html")
+	
+	http.HandleFunc("/", handler.GetArtists)
+	http.HandleFunc("/details", handler.GetLocations)
 
-	http.HandleFunc("/", GetArtists)
 
-	fs := http.FileServer(http.Dir("static"))
+	fs := http.FileServer(http.Dir("/web/static"))
 	http.Handle("/static/", http.StripPrefix("/static", fs))
-
+	
+	// if err!=nil{
+	// 	log.Fatal(err)
+	// }
 	fmt.Println("Listening on :8001...")
 	http.ListenAndServe(":8001", nil)
 }
