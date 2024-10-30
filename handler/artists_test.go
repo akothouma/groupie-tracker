f10426e53a80abf0d1c34e55802a181db03dc108
David Ochiel <nguonodave@gmail.com>
2024-10-30 11:04:36 +0300

    feat(tests): add test for MoreDetails func

--- handler/artists_test.go
+++ handler/artists_test.go
@@ -46,3 +46,48 @@ func TestGetArtists(t *testing.T) {
 		})
 	}
 }
+
+func TestMoreDetails(t *testing.T) {
+	tmpl, err := template.ParseGlob("../" + vars.Template_dir + "/*.html")
+	if err != nil {
+		t.Errorf("Failed to parse templates: %v", err)
+	}
+	vars.Templates = tmpl
+
+	tests := []struct {
+		name                 string
+		query_id             string
+		method               string
+		expected_status_code int
+	}{
+		{
+			name:                 "GET with valid JSON",
+			query_id:             "1",
+			method:               http.MethodGet,
+			expected_status_code: http.StatusOK,
+		},
+		{
+			name:                 "non-GET method",
+			query_id:             "1",
+			method:               http.MethodPost,
+			expected_status_code: http.StatusMethodNotAllowed,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			request := httptest.NewRequest(tc.method, "/artist", nil)
+
+			q := request.URL.Query()
+			q.Add("id", tc.query_id)
+			request.URL.RawQuery = q.Encode()
+
+			w := httptest.NewRecorder()
+			MoreDetails(w, request)
+
+			if w.Code != tc.expected_status_code {
+				t.Errorf("expected %v, got %v", tc.expected_status_code, w.Code)
+			}
+		})
+	}
+}
