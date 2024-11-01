41fc18382374ad7c9cbe6ee5ddcb733f8032362b
David Ochiel <nguonodave@gmail.com>
2024-11-01 18:29:11 +0300

    feat(error handling): add check for when error template is not available

--- errorHandling/error.go
+++ errorHandling/error.go
@@ -3,6 +3,7 @@ package errorhandling
 import (
 	"net"
 	"net/http"
+	"os"
 	"time"
 
 	"groupie/vars"
@@ -11,28 +12,30 @@ import (
 // handles HTTP errors by writing the specified status code and error message
 // to the response writer, and then rendering an error template with the provided data.
 func RenderErr(w http.ResponseWriter, statusCode int, header string, errMessage string) {
+	if _, err := os.Stat(vars.Template_dir + "errors.html"); os.IsNotExist(err) {
+		http.Error(w, "Currently unable to display this page. Please try again later.", http.StatusInternalServerError)
+		return
+	}
+
 	w.WriteHeader(statusCode)
 
 	// data to pass to the error template
 	data := struct {
 		StatusCode int
-		Header string
+		Header     string
 		Message    string
 	}{
 		StatusCode: statusCode,
-		Header: header,
+		Header:     header,
 		Message:    errMessage,
 	}
 
 	// render the page
-	errorsTmplErr := vars.Templates.ExecuteTemplate(w, "errors.html", &data)
-	if errorsTmplErr != nil {
-		http.Error(w, "Currently unable to display this page. Please try again later.", http.StatusInternalServerError)
-	}
+	vars.Templates.ExecuteTemplate(w, "errors.html", &data)
 }
 
 func ClientConnected() bool {
-    timeout := 7 * time.Second
-    _, err := net.DialTimeout("tcp", "8.8.8.8:53", timeout)
-    return err == nil
+	timeout := 7 * time.Second
+	_, err := net.DialTimeout("tcp", "8.8.8.8:53", timeout)
+	return err == nil
 }
